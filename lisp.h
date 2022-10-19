cd231321f1930f0c6019600348c407ffa5aab8ea
cycle <cycle@uber.space>
2022-10-19 15:04:15 +0200

    add scaffolding for json output

--- lisp.h
+++ lisp.h
@@ -2,12 +2,74 @@
 #ifndef _LISP_H_
 #define _LISP_H_
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef uint64_t u64;
+
+typedef u64 Expr;
+
+Expr make_expr(u64 type, u64 data);
+u64 expr_type(Expr exp);
+u64 expr_data(Expr exp);
+
+enum
+{
+    TYPE_NIL = 0,
+    TYPE_SYMBOL,
+    TYPE_CONS,
+    TYPE_STRING,
+};
+
+enum
+{
+    DATA_NIL = 0,
+};
+
+#define nil 0
+
+inline static bool is_nil(Expr exp)
+{
+    return exp == nil;
+}
+
+inline static bool is_symbol(Expr exp)
+{
+    return expr_type(exp) == TYPE_SYMBOL;
+}
+
+inline static bool is_cons(Expr exp)
+{
+    return expr_type(exp) == TYPE_CONS;
+}
+
+inline static bool is_string(Expr exp)
+{
+    return expr_type(exp) == TYPE_STRING;
+}
+
 #endif /* _LISP_H_ */
 
 #ifdef LISP_IMPLEMENTATION
 
 #ifndef _LISP_C_
 #define _LISP_C_
+
+Expr make_expr(u64 type, u64 data)
+{
+    return (data << 8) | (type & 255);
+}
+
+u64 expr_type(Expr exp)
+{
+    return exp & 255;
+}
+
+u64 expr_data(Expr exp)
+{
+    return exp >> 8;
+}
 
 #endif /* _LISP_C_ */
 
--- sexp2json.c
+++ sexp2json.c
@@ -1,8 +1,57 @@
 
 #include "lisp.h"
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <inttypes.h>
+
+#define FAIL(...) fail(__FILE__, __LINE__, __VA_ARGS__)
+
+static void fail(char const * file, int line, char const * fmt, ...)
+{
+    FILE * out = stderr;
+    fprintf(out, "%s:%d: [FAIL] ", file, line);
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(out, fmt, args);
+    va_end(args);
+    exit(1);
+}
+
+static bool maybe_read_expr(Expr * pexp)
+{
+    *pexp = nil;
+    return false;
+}
+
+static void render_expr(Expr exp)
+{
+    switch (expr_type(exp))
+    {
+    default:
+        FAIL("cannot render expression of type %" PRIx64 "\n", expr_type(exp));
+        break;
+    }
+}
+
+static void sexp2json()
+{
+    Expr exp;
+    if (!maybe_read_expr(&exp))
+    {
+        /* map empty stream => empty stream */
+        return;
+    }
+
+    render_expr(exp);
+    printf("\n");
+}
+
 int main(int argc, char ** argv)
 {
+    sexp2json();
     return 0;
 }
 
